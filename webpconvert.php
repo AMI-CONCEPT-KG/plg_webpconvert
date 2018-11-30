acef4d0573b46b56f0b43ff8bed3eae2ba337f33
Travis Risner <travis@bluewall.com>
2018-11-30 16:22:05 -0600

    finalize initial version for testing

--- webpconvert.php
+++ webpconvert.php
@@ -22,6 +22,7 @@
      */
     protected $app;
     protected $webp_support = false;
+    protected $quality = 100;
 
     /**
      * Fetches a reference to Joomla's application instance and calls the
@@ -59,12 +60,12 @@
     //Ensure the body was successfully parsed
     if ($body instanceof \SimpleXMLElement) {
       //Convert images to WebP and update body
-      $body = $this->addWebPToBody($body);
+      $body = $this->addWebpToBody($body);
       //Set the body
       $this->app->setBody($body);
     }
   }
-  
+
   protected function webpAllowed(): bool {
     //Check to see if imagewebp function exists
     if (\function_exists('imagewebp')) {
@@ -74,8 +75,29 @@
     }
   }
 
-  protected function addWebPToBody(\SimpleXMLElement $body): string {
-
+  protected function addWebpToBody(\SimpleXMLElement $body): string {
+    //Locate all possible images resources
+    $images = $body->xpath('//img[@src]');
+    $converted_images = [];
+    $i = 0;
+    //Loop through all images
+    foreach ($images as &$image) {
+      //Store the original filename
+      $converted_images[$i]['original'] = (string) $image['src'];
+      //Run image through conversion script
+      $image = $this->processImage($image['src']);
+      //Store the webp filename
+      $converted_images[$i]['webp'] = $image;
+      $i++;
+    }
+    //Convert XML Element back to string
+    $body = $body->asXML();
+    //Loop through the converted images array
+    foreach($converted_images as $image) {
+      //replace original filename with webp filename
+      $body = \str_replace($image['original'], $image['webp'], $body);
+    }
+    return $body;
   }
 
   /**
@@ -115,5 +137,44 @@
     }
     // Check if the document body was parsed and converted successfully
     return $document instanceof \SimpleXMLElement ? $document : NULL;
-  }  
+  }
+
+  protected function processImage(string $image): ?string {
+    //Convert file extension to webp
+    $webp_path = \substr_replace($image, 'webp', \strrpos($image, '.') +1);
+    //$webp_path = \dirname($webp_path).'/webp/'.\basename($webp_path);
+    //Check for the existence of a webp
+    if(!$this->webpExists($webp_path)) {
+      //Run the conversion
+      $this->convertImage($image, $webp_path);
+    }
+    return $webp_path;
+  }
+
+  protected function webpExists(string $image): bool {
+    //If file exists
+    if(\is_file(JPATH_ROOT.'/'.$image)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  protected function convertImage(string $origin, string $dest): bool {
+    //Check to ensure origin file exists
+    if(\is_file($origin)) {
+      //Create an image resource from the origin
+      $image_resource = \imagecreatefromstring(\file_get_contents($origin));
+      //Return true if conversion is possible
+      if(\imagewebp($image_resource, $dest, $this->quality)) {
+        return true;
+      } else {
+        //Return false on error
+        return false;
+      }
+    } else {
+      //Return false is origin doesn't exist
+      return false;
+    }
+  }
 }
